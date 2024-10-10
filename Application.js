dea4bd4950b114e8b4f3fb8dced04a0aa49ae7d7
Krissapter <kevenrud@hotmail.no>
2024-10-10 17:01:14 +0200

    Refactored some code

--- Application.js
+++ Application.js
@@ -1,6 +1,6 @@
 var appInfo = "PlantTempSurveilance https://github.com/Krissapter/TemperatureSurveilance";
 
-var url = 'https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=55.4514&lon=37.3715';
+var url = 'https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=70.5804&lon=-8.4041';
 
 //TODO Transition from running as a script to like a server.
 //TODO Make the application run the fetch using setTimeout
@@ -22,6 +22,7 @@ async function runBullshit(){
     var subTenTime = [];
     let i = 0;
     nineDayForecast.forEach(hour => {
+        console.log(hour.data.instant.details.air_temperature)
         if(hour.data.instant.details.air_temperature < 10){
             if(hour.data.instant.details.air_temperature <= 0){
                 freezeTempsTime[i] = hour.time;
@@ -31,35 +32,31 @@ async function runBullshit(){
             i++;
         }
     });
-
-    const subTenDay = subTenTime.reduce((acc, e) => {
-        let day = e.slice(0, 10);
-        let time = e.slice(11, 16);
-        acc[day] = acc[day] || [];
-        acc[day].push(time);
-        return acc;
-    }, {});
-    
-    //console.log(subTenDay);
-    
-    const freezeTempDay = freezeTempsTime.reduce((acc, e) => {
-        let day = e.slice(0, 10);
-        let time = e.slice(11, 16);
-        acc[day] = acc[day] || [];
-        acc[day].push(time);
-        return acc;
-    }, {});
+    if (freezeTempsTime.length >= 1){
+        temperatureWarning(organizeColdDays(freezeTempsTime), 0);
+    }else if(subTenTime.length >= 1){
+        temperatureWarning(organizeColdDays(subTenTime), 10)
+    }
 
     //TODO Further expand on data processing for different temperature thresholds.
 
-    tenDegreeLimit(subTenDay);
     //TODO Set up notification system to send a mail if a set treshold is reached.
+}
+
+function organizeColdDays(rawDays){
+    let coldDays = rawDays.reduce((acc, e) => {
+        let day = e.slice(0, 10);
+        let time = e.slice(11, 16);
+        acc[day] = acc[day] || [];
+        acc[day].push(time);
+        return acc;
+    }, {});
+    return coldDays;
 }
 
 //TODO Check if temp is below treshold in any of the days in the list
 
-//Some plants are generally hardy down to only 10°C
-function tenDegreeLimit(days){
+function temperatureWarning(days, limit){
     let i = 0;
     var dayArr = Object.keys(days);
     dayArr.forEach(day => {
@@ -67,6 +64,6 @@ function tenDegreeLimit(days){
             i++;
         }
     });
-    console.log("\nThe next 11 days may contain " + dayArr.length + " days below 10°C, starting with " + dayArr[0] + ".\n" + i +" of these are within the next 3 days, and has a high probability of being below 10°C. \nConsider taking action for less hardy plants.");
+    console.log("\nThe next 11 days may contain " + dayArr.length + " days below "+ limit + "°C, starting with " + dayArr[0] + ".\n" + i +" of these are within the next 3 days, and has a high probability of being below 10°C. \nConsider taking action for less hardy plants.");
 }
 runBullshit();
